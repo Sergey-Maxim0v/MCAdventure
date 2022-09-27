043d2ddf30f80f995226a5bac596ee25efe2f88e
Seregey_Maximov <dev9092336062@gmail.com>
2022-09-27 12:06:34 +0300

    MainLayout - styles

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -3,10 +3,15 @@ import {IMainLayout} from "./types";
 import Navigation from "../Navigation";
 
 const MainLayout: FC<IMainLayout> = ({children}) => (
-    <>
+    <div className={'w-screen h-screen flex flex-col'}>
       <Navigation/>
-      {children}
-    </>
+
+      <div className={'w-full flex flex-grow items-center justify-center box-border'}>
+        <div className={'w-[971px] h-[646px] rounded-[32px] text-white relative bg-[#14161C]'}>
+          {children}
+        </div>
+      </div>
+    </div>
 )
 
 export default MainLayout
