5662cb2d68e605b64d2cde5780467360829c011c
Seregey_Maximov <dev9092336062@gmail.com>
2022-09-27 11:22:10 +0300

    useLocation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,11 +1,10 @@
 import {FC} from "react";
 import {ROUTES, RoutesType} from "../../constants/routes";
 import NavigationButton from "./components/NavigationButton";
+import {useLocation} from "react-router-dom";
 
 const Navigation: FC = () => {
-
-  // TODO: достать url и по нему выделить нужный элемент
-  const url = ''
+  const {pathname} = useLocation()
 
   const linkList = Object.keys(ROUTES) as Array<keyof RoutesType>
 
@@ -13,7 +12,7 @@ const Navigation: FC = () => {
       <div
           className={'w-full h-[60px] flex flex-nowrap items-center justify-around align-middle text-white uppercase bg-[#181A20]'}>
         {linkList.map((link) => (
-            <NavigationButton key={link} link={link} isActive={false}/>
+            <NavigationButton key={link} link={link} isActive={ROUTES[link] === pathname}/>
         ))}
       </div>
   );
