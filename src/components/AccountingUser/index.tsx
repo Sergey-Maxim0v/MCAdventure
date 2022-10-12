1aa24b43da792d81b95310991b51ad5603e72d7f
Seregey_Maximov <dev9092336062@gmail.com>
2022-10-12 15:56:09 +0300

    AccountingUser

--- src/components/AccountingUser/index.tsx
+++ src/components/AccountingUser/index.tsx
@@ -1,13 +1,23 @@
 import {FC} from "react";
+import classNames from "classnames";
+import styles from './styles.module.scss'
 
 const AccountingUser: FC = () => {
   return (
       <div
-          className={'w-full py-16 pl-40 pr-16 flex justify-center items-center'}
+          className={'w-full p-16 flex justify-center items-center'}
       >
         <div
-            className={''}
+            className={classNames('w-[546px] h-80 rounded-3xl flex', styles.row)}
         >
+          <div
+              className={classNames(
+                  'w-1/2 pt-6 rounded-3xl flex flex-col justify-between items-center',
+                  styles.row
+              )}
+          >
+
+          </div>
 
         </div>
       </div>
